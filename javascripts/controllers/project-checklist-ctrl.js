f4eff5f2d5b7d92c4171b216ec5df54563104da6
Tom <basictom@githu.com>
2017-08-22 17:28:12 -0500

    working on ng-repeat with checkboxes

--- javascripts/controllers/project-checklist-ctrl.js
+++ javascripts/controllers/project-checklist-ctrl.js
@@ -6,5 +6,55 @@ app.controller("ProjectChecklist", function($scope, $rootScope, $location, Proje
     });
   });
 
+  $scope.assetsObject = [
+    {
+    question: "Blah Blah Blah",
+    cb1: true,
+    cb2: false,
+    cb3: false,
+    cb4: false,
+    cb5: false
+  },
+  {
+    question: "YA YA YA YA",
+    cb1: false,
+    cb2: true,
+    cb3: false,
+    cb4: false,
+    cb5: false
+  },
+  {
+    question: "OH OHO OHO",
+    cb1: false,
+    cb2: false,
+    cb3: false,
+    cb4: false,
+    cb5: false
+  }
+];
+$scope.checkedItems = () => {
+    var checkItems = [];
+    angular.forEach($scope.assetsObject, function(objs, arrayIndex){
+        angular.forEach(objs, function(cb, key) {
+          console.log(cb);
+          console.log(key);
+            if(key.substring(0, 2) == "cb" && cb) {
+                checkItems.push(objs.question + '-' + key)
+            }
+        })
+    })
+    return checkItems
+}
+
+
+
+
+
+
+
+  // $scope.submitChecklist = () => {
+  //   console.log($scope.oneAssets);
+  // }
+
 
 });
