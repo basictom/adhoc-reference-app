39631a6d88a9433ae4d5ce431a4e6837321af7e4
Tom <basictom@githu.com>
2017-09-12 17:34:41 -0500

    allowing user checkboxes to store inside objects

--- javascripts/controllers/project-checklist-ctrl.js
+++ javascripts/controllers/project-checklist-ctrl.js
@@ -1,17 +1,5 @@
 app.controller("ProjectChecklist", function($scope, $rootScope, $location, ProjectFactory){
 
-
-  // $scope.questionsArray = [
-  //       "PSD - Copied to the Server",
-  //       "HTML",
-  //       "Text",
-  //       "Images",
-  //       "Cell Id",
-  //       "ASF / Marketing Plan / PID File",
-  //       "Grid"
-  // ];
-
-
   var ctrl = this;
 
   this.fields = [
@@ -26,8 +14,7 @@ app.controller("ProjectChecklist", function($scope, $rootScope, $location, Proje
 
   this.versions = [{}];
 
-	// This function really just fills in the blanks
-  this.formatForApi = function() {
+  this.userResponse = function() {
 		return this.versions
       .map(function(version) {
         return ctrl.fields.reduce(function(out, field) {
@@ -37,55 +24,8 @@ app.controller("ProjectChecklist", function($scope, $rootScope, $location, Proje
       });
   };
 
-  let userOutput = ctrl.formatForApi();
-
   $scope.submitChecklist = () => {
-    console.log(userOutput);
+    console.log(this.userResponse());
   };
-
-
-
-
-  // $scope.checkboxArray = [];
-  // let key = $scope.checkboxArray;
-  // let obj = [];
-  // $scope.count = 0;
-  // $scope.addNewCheckbox = () => {
-  //   $scope.versionBtn = true;
-  //   let newKey = $scope.count++;
-  //   let pushKey = "cb" + newKey;
-  //   console.log(pushKey);
-  //   obj[pushKey] = false;
-  //   key.push(obj);
-  //   console.log(obj);
-  // };
-  //
-  // $scope.onCbChange = (cb, id) => {
-  //   console.log("changinggg, index.", cb);
-  //   console.log("changinggg, id.", id);
-  // };
-
-
-
-
-
-  $scope.checkedItems = () => {
-      var checkItems = [];
-      angular.forEach($scope.assetsObject, function(objs, arrayIndex){
-          angular.forEach(objs, function(cb, key) {
-              if(key.substring(0, 2) == "cb" && cb) {
-                  checkItems.push(objs.question + '-' + key)
-              }
-          })
-      })
-      return checkItems
-  }
-
-
-
-  // $scope.submitChecklist = () => {
-  //   console.log($scope.oneAssets);
-  // }
-
 
 });
